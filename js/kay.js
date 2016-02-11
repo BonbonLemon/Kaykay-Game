958a5b029eff8db6a24ed22c7e353ee5fbfd0303
Stephen Chen <chen.stephen.r@gmail.com>
2016-02-10 20:16:39 -0800

    kay moves

--- js/kay.js
+++ js/kay.js
@@ -14,26 +14,69 @@
 
   Kay.prototype.bindMovement = function () {
     document.onkeydown = function (e) {
-      console.log(e.which);
-    };
+      switch (e.which) {
+        case 37:
+          this.dir[0] = -6;
+          break;
+        case 38:
+          this.dir[1] = -4;
+          break;
+        case 39:
+          this.dir[0] = 6;
+          break;
+        case 40:
+          this.dir[1] = 4;
+          break;
+        case 32:
+          console.log(this.pos);
+        default:
+
+      }
+    }.bind(this);
 
     document.onkeyup = function (e) {
-      console.log("up");
-    };
+      switch (e.which) {
+        case 37:
+        case 39:
+          this.dir[0] = 0
+          break;
+        case 38:
+        case 40:
+          this.dir[1] = 0
+          break;
+        default:
+
+      }
+    }.bind(this);
 
     setInterval(function () {
       this.move();
-    }.bind(this), 200)
+    }.bind(this), 30)
   };
 
   Kay.prototype.move = function () {
-    this.ctx.clearRect(0, 0, 75, 140);
+    this.ctx.clearRect(this.pos[0], this.pos[1], 75, 140);
     this.pos[0] += this.dir[0];
     this.pos[1] += this.dir[1];
+
+    if (this.pos[0] < -25) {
+      this.pos[0] = 710;
+    } else if (this.pos[0] > 750) {
+      this.pos[0] = 10;
+    }
+
+    if (this.pos[1] < -35) {
+      this.pos[1] = 450;
+    } else if (this.pos[1] > 490) {
+      this.pos[1] = 10
+    }
+    
     this.render();
   };
 
   Kay.prototype.render = function () {
-
+    var imageObj = new Image();
+    imageObj.src = this.image;
+    this.ctx.drawImage(imageObj, this.pos[0], this.pos[1]);
   };
 })();
